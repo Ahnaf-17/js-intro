24e186201c790b0df3decf50c16d6339f1751495
unknown <ahnaf.ahmed.42173@gmail.com>
2023-07-29 23:09:49 +0600

    added

--- array.js
+++ array.js
@@ -1,3 +1,7 @@
 var Ages = [11 ,21,45,17,25,23,6 ];
 var fee = [5000,2000,4000,150];
 var name = ['ahnaf','ahmed'];
+var vowel = ['a' , 'e', 'i', 'o', 'u'];
+console.log(vowel);
+console.log("Length of Age :"+Ages.length);
+
