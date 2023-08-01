5ade9ec6bf4416a4b94e0c5026e4f3bead2284ad
unknown <ahnaf.ahmed.42173@gmail.com>
2023-08-01 21:26:06 +0600

    function added

---
+++ function.js
@@ -0,0 +1,12 @@
+// function
+
+function food(){
+    console.log("- pizza");
+    console.log("- burger");
+    console.log("- cold coffee");
+    console.log("- milk shake");
+    console.log("- chicken fry");
+}
+
+// call the function 
+food();
--- while.js
+++ while.js
@@ -1,5 +1,26 @@
-var member = 0;
-while(member<10){
-    member++;
-    console.log(member);
+// var member = 0;
+// while(member<10){
+//         console.log(member);
+
+//     member++;
+// }
+
+// var i = 0;
+// for (i=0; i<5; i++){};
+// console.log(i);
+
+// var marks = [ 13, 15, 14, 20, 18];
+// for(var i=0; i<marks.length; i++){
+//  if(marks[i] >=15){
+//   continue;
+//  }
+//  console.log(marks[i]);
+// };
+
+// var name = "false"; 
+// console.log(typeof name );
+
+var i = 5;
+for (; i < 5; i++) {
+ console.log(i);
 }
