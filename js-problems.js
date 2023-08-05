4f2a3283b5ecbfc8bed275ecf03299a3fce57e11
unknown <ahnaf.ahmed.42173@gmail.com>
2023-08-05 21:47:32 +0600

    added

---
+++ js-problems.js
@@ -0,0 +1,9 @@
+// const people = [10,11,12,13,15,14,21,11]
+// const partial = people.slice(2,5);
+// console.log(partial)
+
+// splice 
+
+const people2 = [10,11,12,13,15,14,21,11]
+const partial2 = people2.splice(2,3, 99, 555);
+console.log(partial2);
